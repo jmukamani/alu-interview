2fcb71e04f729cab00ab0f37540c39f615b307bc
jmukamani <j.mukamani>
2023-09-12 12:29:15 -0700

    operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,20 +4,29 @@ write a method that calculates the fewest number of operations
 needed to result in exactly n H characters in the file
 """
 
+
 def minOperations(n):
-    if n <= 1:
-        return 0
+    """
+        Calculates the fewest number of operations needed
+        to result in exactly n H characters in the file
 
-    # Initialize a list to store the minimum operations for each position.
-    dp = [0] * (n + 1)
+        Args:
+            n: repetitions of H
 
-    for i in range(2, n + 1):
-        # Initialize the minimum operations for this position to be i.
-        dp[i] = i
-        for j in range(2, i):
-            # If i is divisible by j (j is a factor of i), we can try to copy
-            # j characters and then paste (i // j) times to get i characters.
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + 1 + (i // j - 1))
+        Returns:
+            number of operations (Copy & Paste) to reach n Hs
+    """
+    operations = 0
+    summation = 1
+    carrier = 0
 
-    return db[n]
+    while summation < n:
+        if n % summation == 0:
+            carrier = summation
+            summation *= 2
+            operations += 1
+        else:
+            summation += carrier
+        operations += 1
+
+    return operations
